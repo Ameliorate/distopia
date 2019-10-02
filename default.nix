8456692a57d9b24ebd472258a4c859926ebdf20a
Amelorate <ameilorate2@gmail.com>
2019-10-02 14:09:41 -0400

    Add building with nix

---
+++ default.nix
@@ -0,0 +1,23 @@
+let pkgs = import <nixpkgs> { }; in
+
+let
+  perl = pkgs.perl.withPackages (perl-packages: with perl-packages; [
+    # perl packages
+    FileSlurp
+  ]); 
+  distopia-pl = pkgs.stdenv.mkDerivation {
+    name = "distopia-pl";
+    src = ./.;
+
+    installPhase = ''
+      mkdir -p $out/lib/distopia
+      cp -r src $out/lib/distopia
+      cp -r pipes $out/lib/distopia
+    '';
+  }; in 
+pkgs.writeShellScriptBin "distopia" ''
+  export PATH=$PATH:${pkgs.xsel}/bin:${pkgs.dmenu}/bin:${pkgs.libnotify}/bin:${pkgs.cowsay}/bin
+  DISTOPIA_PWD="${distopia-pl}/lib/distopia"
+  export DISTOPIA_PWD
+  ${perl}/bin/perl ${distopia-pl}/lib/distopia/src/main.pl $1
+''
--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-DISTOPIA_PWD="$(dirname "$(readlink -f "$0")")"
+DISTOPIA_PWD="$(dirname "$0")"
 export DISTOPIA_PWD
 nix-shell $DISTOPIA_PWD/shell.nix --command "$DISTOPIA_PWD/src/main.pl $1"
